b2b57f844df9b0a359af89b87e848cbd634e2b8f
Brad Larson <bhlarson@gmail.com>
2023-04-22 13:06:25 -0700

    Tag version 0.0.18

--- config/config.sh
+++ config/config.sh
@@ -1,3 +1,3 @@
-export VERSION=0.0.17
-export job=localhost:32000/job:0.0.17
-export dev=localhost:32000/dev:0.0.17
+export VERSION=0.0.18
+export job=localhost:32000/job:0.0.18
+export dev=localhost:32000/dev:0.0.18
